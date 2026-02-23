3d3465811cf322476969df2fac935355437405bb
rishi <pandeyrishiv@gmail.com>
2026-02-23 19:53:25 +0000

    Feat(src): create ticket aggregation function

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -24,5 +24,42 @@ export default class TicketService {
         }
     }
 
+    #aggregateTickets(ticketTypeRequests) {
+        let adultCount = 0;
+        let childCount = 0;
+        let infantCount = 0;
+
+        return ticketTypeRequests.reduce(
+            (acc, request) => {
+                const type = request.getTicketType();
+                const count = request.getNoOfTickets();
+
+                if (count <= 0) {
+                    throw new InvalidPurchaseException(
+                        "Ticket count must be greater than zero",
+                    );
+                }
+
+                switch (type) {
+                    case "ADULT":
+                        acc.adultCount += count;
+                        break;
+                    case "CHILD":
+                        acc.childCount += count;
+                        break;
+                    case "INFANT":
+                        acc.infantCount += count;
+                        break;
+                    default:
+                        throw new InvalidPurchaseException(
+                            "Unknown ticket type",
+                        );
+                }
+
+                return acc;
+            },
+            { adultCount: 0, childCount: 0, infantCount: 0 },
+        );
+    }
   }
 }
